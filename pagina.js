21ec3122242dd54df6a2124c96d123d4f8033cbd
samueloc123 <72421709+samueloc123@users.noreply.github.com>
2021-04-16 22:39:57 -0500

    Add files via upload

---
+++ pagina.js
@@ -0,0 +1,21 @@
+var uno=function(){
+	var n1=document.getElementById("entregas");
+	var n2=document.getElementById("proximamente");
+
+	console.log("hola");
+
+	n1.className="entregass";
+		n2.className="imano";
+
+}
+
+var dos=function(){
+	var n3=document.getElementById("entregas2");
+	var n4=document.getElementById("proximamente");
+
+	console.log("hola");
+
+	n3.className="entregass2";
+		n4.className="imano";
+
+}
